9e3c8e6c0e4c199f1b8a96afd371f1ca476e5e15
bdvhtva <bdvhtva@outlook.com>
2025-09-27 03:58:41 +0000

    Add marp.sh

---
+++ marp.sh
@@ -0,0 +1,2 @@
+npx @marp-team/marp-cli@latest --html --bespoke.transition main.md --pptx
+docker run --rm -v "$PWD:/home/marp/app" ghcr.io/marp-team/marp-cli:latest --html --bespoke.transition main.md --pptx
